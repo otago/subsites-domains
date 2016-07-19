f9b7a6d682f2564d57a87ba2c8c15000c4e0ef0b
torleif <torleifw@gmail.com>
2016-07-19 16:32:07 +1200

    Fix: added config back in

--- _config.php
+++ _config.php
@@ -1,1 +1,7 @@
 <?php
+
+define('SUBSITE_DOMAIN_DIR', ltrim(Director::makeRelative(realpath(__DIR__)), DIRECTORY_SEPARATOR));
+
+
+Object::add_extension('HtmlEditorField_Toolbar', 'HtmlEditorField_ToolbarExtension');
+ShortcodeParser::get('default')->register('subsite_link', array('HtmlEditorField_ToolbarExtension', 'link_shortcode_handler'));

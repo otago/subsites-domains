c326faa0bd9e008804cc388570758cdc7b7001bb
Torleif West <torleif.west@op.ac.nz>
2019-03-15 11:01:56 +1300

    config update

--- _config.php
+++ _config.php
@@ -1,9 +1,8 @@
 <?php
 
+namespace OP;
+
 use SilverStripe\View\Parsers\ShortcodeParser;
 
-//Object::add_extension('HtmlEditorField_Toolbar', 'HtmlEditorField_ToolbarExtension');
-ShortcodeParser::get('default')->register('subsite_link', '\\OP\\ModalControllerExtension::link_shortcode_handler');
-
-
+ShortcodeParser::get('default')->register('subsite_link', [ModalControllerExtension::class, 'link_shortcode_handler']);
 

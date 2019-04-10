9073eda929519222ec581633f73e795da51957ff
torleif <torleifw@gmail.com>
2019-04-11 10:53:12 +1200

    Update _config.php

--- _config.php
+++ _config.php
@@ -4,5 +4,10 @@ namespace OP;
 
 use SilverStripe\View\Parsers\ShortcodeParser;
 
-ShortcodeParser::get('default')->register('subsite_link', [ModalControllerExtension::class, 'link_shortcode_handler']);
+ShortcodeParser::get('default')->register('subsite_link', [ModalControllerExtension::class, 'link_shortcode_handler']
+);
 
+
+ShortcodeParser::get('default')->register(
+        'sitetree_link', [ModalControllerExtension::class, 'link_shortcode_handler']
+);
